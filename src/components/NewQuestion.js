f6a76e805f2ab917a2ac09e5505ef66e4800db40
Iulia <iuliaelizamts@gmail.com>
2020-11-19 12:02:43 +0100

    fixed error at saving new question

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -15,13 +15,14 @@ class NewQuestion extends React.Component {
     const option1text = option1.value;
     const option2 = document.getElementById('option2');
     const option2text = option2.value;
-    const author = this.props.loggedInUser.id;
     const newQuestion = {
-      author : author,
+      author : this.props.loggedInUser,
       optionOneText : option1text,
       optionTwoText : option2text
     };
+
     this.props.dispatch(saveQuestion(newQuestion));
+    alert('your question was saved');
   }
 
   render (){
@@ -41,7 +42,7 @@ class NewQuestion extends React.Component {
             <h3 className="questionText"> Would you rather ... </h3>
             <input id="option1" type="text" placeholder="option one"></input>
             <input id="option2" type="text" placeholder="option two"></input>
-            <button className="saveBtn" onClick={this.saveNewQuestion}>Save</button>
+            <input type="button" id="saveBtn" value="Save question" onClick={this.saveNewQuestion}></input>
           </form>
           <Footer/>
         </React.Fragment>
